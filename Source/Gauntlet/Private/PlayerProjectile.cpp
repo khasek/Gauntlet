4d1fb1f1dee62976ef70ceb49a9e98dcf8a88104
Kendal Hasek <93564097+khasek@users.noreply.github.com>
2025-05-15 02:47:01 -0700

    Merging part 1

---
+++ Source/Gauntlet/Private/PlayerProjectile.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerProjectile.h"
+
+// Sets default values
+APlayerProjectile::APlayerProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	InitialLifeSpan = 2.0f;
+}
+
+// Called when the game starts or when spawned
+void APlayerProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	FVector NewLocation = GetActorLocation() + MoveDirection * Speed * DeltaTime;
+	FHitResult Hit;
+
+	SetActorLocation(NewLocation, true, &Hit);
+
+	if (Hit.bBlockingHit) {
+		AActor* HitActor = Hit.GetActor();
+		if (HitActor && HitActor->GetClass()->ImplementsInterface(UBPI_TakeDamage::StaticClass())) {
+			IBPI_TakeDamage::Execute_ApplyDamage(HitActor, Damage);
+		}
+		Destroy();
+	}
+}
+
+void APlayerProjectile::Init(const FVector& Direction)
+{
+	MoveDirection = Direction.GetSafeNormal();
+}
